4a76e8ba59da2caeb4d373778bb26790fd57964c
bukubukutyagamaguti <bukubukutyagamaguti@gmail.com>
2022-10-11 23:26:43 +0900

    1011

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,73 @@
-use anyhow::Context;
+mod repositories;
+mod handlers;
+
+use crate::repositories::{
+    TodoRepository,
+    TodoRepositoryForMemory
+};
+
 use axum::{
-    http::StatusCode,
-    response::IntoResponse,
+    extract::Extension,
     routing::{get, post},
-    Json,
     Router
 };
-use serde::{Deserialize, Serialize};
-use std::{
-    net::SocketAddr,
-    collections::HashMap,
-    env,
-    sync::{Arc, RwLock}
-};
-use thiserror::Error;
 
-#[derive(Debug, Error)]
-enum RepositoryError {
-    #[error("NotFound, id is {0}")]
-    NotFound,
+use handlers::create_todo;
+
+use std::net::SocketAddr;
+use std::{
+    // Arc 共有所有権に関して
+    // RwLock リーダ、ライター権限をロック
+    env,
+    sync::Arc
+};
+
+#[tokio::main]
+async fn main() {
+    // logging
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
+    let repository = TodoRepositoryForMemory::new();
+    let app = create_app(repository);
+    let addr = SocketAddr::from(([127,0,0,1], 3000));
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-fn main() {
-    
+pub fn create_app<T: TodoRepository>(repository: T) -> Router {
+    Router::new()
+        .route("/", get(root))
+        .route("/todos", post(create_todo::<T>))
+        .layer(Extension(Arc::new(repository)))
+}
+
+async fn root() -> &'static str {
+    "heeeeeeee"
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use axum::{
+        body::Body
+    };
+    use axum::http::Request;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let repository = TodoRepositoryForMemory::new();
+        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        assert_eq!(body, "heeeeeeee");
+    }
 }
